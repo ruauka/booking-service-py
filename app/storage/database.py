8de20deb85b24a407b17ea134eb6890aeea5b7b0
ruauka <ruauka123@yandex.ru>
2023-04-20 18:05:52 +0300

    add async_sessionmaker

--- app/storage/database.py
+++ app/storage/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, declared_attr
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy import Column, Integer
 
 from config import cfg
@@ -7,7 +7,7 @@ from config import cfg
 # асинхронный движок алхимии
 engine = create_async_engine(cfg.db_url)
 # асинхронный генератор сессий для бд
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 # асинсхронная сессия для бд
