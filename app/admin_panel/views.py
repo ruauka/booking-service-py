247b30c17ea5d3694f765e7dc244b59f04451d9f
ruauka <ruauka123@yandex.ru>
2023-05-22 19:12:08 +0300

    add doc strings to admin views

--- app/admin_panel/views.py
+++ app/admin_panel/views.py
@@ -7,6 +7,9 @@ from app.models.room import Room
 
 
 class UserAdmin(ModelView, model=User):
+    """
+    Класс для админки. Вьюха пользователей.
+    """
     column_list = [User.id, User.email, User.booking]
     column_details_exclude_list = [User.hashed_password]
     can_delete = False
@@ -18,23 +21,32 @@ class UserAdmin(ModelView, model=User):
 
 
 class HotelAdmin(ModelView, model=Hotel):
+    """
+    Класс для админки. Вьюха гостиницы.
+    """
     column_list = [c.name for c in Hotel.__table__.columns] + [Hotel.rooms]
-    name = "Отель"
-    name_plural = "Отели"
+    name = "Hotel"
+    name_plural = "Hotels"
     icon = "fa-solid fa-hotel"
 
 
 class RoomAdmin(ModelView, model=Room):
+    """
+    Класс для админки. Вьюха номеров.
+    """
     column_list = [c.name for c in Room.__table__.columns] + [Room.hotel, Room.booking]
-    name = "Номер"
-    name_plural = "Номера"
+    name = "Room"
+    name_plural = "Rooms"
     icon = "fa-solid fa-bed"
 
 
 class BookingAdmin(ModelView, model=Booking):
+    """
+    Класс для админки. Вьюха бронирований.
+    """
     # Booking.__table__.columns - список колонок модели
     # [c.name for c in Booking.__table__.columns] - все названия колонок
     column_list = [c.name for c in Booking.__table__.columns] + [Booking.user]
-    name = "Booking"
+    name = "Bookings"
     name_plural = "Booking"
     icon = "fa-solid fa-book"
